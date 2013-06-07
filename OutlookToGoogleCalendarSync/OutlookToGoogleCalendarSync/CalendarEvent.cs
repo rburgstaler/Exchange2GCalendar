3e084894a4cb76ee475cc4b12c354bd3959570d2
Joris <joris.onghena@ua.ac.be>
2013-06-07 16:58:43 +0200

    Added first version of sync program
    * Adds items to gCal
    * Updates items to gCal

---
+++ OutlookToGoogleCalendarSync/OutlookToGoogleCalendarSync/CalendarEvent.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace OutlookToGoogleCalendarSync
+{
+    /// <summary>
+    /// A calender event item
+    /// </summary>
+    public class CalendarEvent : IEquatable<CalendarEvent>
+    {
+        private string body;
+        private DateTime endDate;
+        private string location;
+        private DateTime startDate;
+        private string subject;
+        private string id;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public CalendarEvent(string id, DateTime startDate, DateTime endDate, string location, string subject, string body)
+        {
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.location = location;
+            this.subject = subject;
+            this.body = body;
+            this.id = id;
+        }
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Body
+        {
+            get { return body; }
+            set { body = value; }
+        }
+
+        public DateTime EndDate
+        {
+            get { return endDate; }
+            set { endDate = value; }
+        }
+
+        public string Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+
+        public DateTime StartDate
+        {
+            get { return startDate; }
+            set { startDate = value; }
+        }
+
+        public string Subject
+        {
+            get { return subject; }
+            set { subject = value; }
+        }
+
+        /// <summary>
+        /// Compare 2 event objects and return true if the id's are equal
+        /// </summary>
+        public bool Equals(CalendarEvent other)
+        {
+            return (other.Id == this.id);
+        }
+
+        /// <summary>
+        /// Compare 2 event objects and return true if they differ in one or more attribute values
+        /// </summary>
+        public bool IsChanged(CalendarEvent other)
+        {
+            if (other.Equals(this))
+            {
+                string testBody = this.body;
+                string testLocation = this.location;
+                string testSubject = this.subject;
+
+                if (testBody == null)
+                    testBody = "";
+
+                if (testLocation == null)
+                    testLocation = "";
+
+                if (testSubject == null)
+                    testSubject = "";
+
+                return (
+                    other.body.Replace("\r", "").Trim('\n').Trim() != testBody.Replace("\r", "").Trim('\n').Trim()
+                    ||
+                    other.endDate != this.endDate
+                    ||
+                    other.location != testLocation
+                    ||
+                    other.startDate != this.startDate
+                    ||
+                    other.subject != testSubject
+                    );
+            }
+            else
+            {
+                throw new Exception("Cannot see if 2 events have changed if they do not share the same ID.");
+            }
+        }
+
+    }
+}
---
+++ OutlookToGoogleCalendarSync/OutlookToGoogleCalendarSync/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+
+namespace OutlookToGoogleCalendarSync
+{
+    internal class Program
+    {
+        private static string user, pass, calendarId;
+
+        private static void Main(string[] args)
+        {
+            ReadConfig();
+
+            CalendarManagerGoogle gManager = new CalendarManagerGoogle(user, pass, calendarId);
+            List<CalendarEvent> events = CalendarManagerOutlook.GetAllEvents();
+            List<CalendarEvent> eventsInGoogle = gManager.GetAllEvents();
+            List<CalendarEvent> eventsProcessed = new List<CalendarEvent>();
+
+            Console.WriteLine("Found {0} events in the outlook calendar", events.Count);
+            CalendarEvent searchEvent;
+
+            foreach (CalendarEvent cEvent in events)
+            {
+                searchEvent = gManager.GetEventFromSyncId(cEvent.Id);
+
+                if (searchEvent == null)
+                {   // -> create a new event
+                    Console.Write("Creating event \"{0}\" ... ", cEvent.Subject);
+                    gManager.CreateEvent(cEvent);
+                    Console.WriteLine("Created");
+                }
+                else if (cEvent.IsChanged(searchEvent))
+                {
+                    // -> update event
+                    Console.Write("Update event \"{0}\" ... ", cEvent.Subject);
+                    gManager.UpdateEvent(cEvent);
+                    Console.WriteLine("Updated");
+                }
+
+                eventsProcessed.Add(cEvent);
+            }
+
+            List<CalendarEvent> eventsNotProcessed = eventsInGoogle.Except<CalendarEvent>(eventsProcessed).ToList<CalendarEvent>();
+            if (eventsNotProcessed.Count > 0)
+            {
+                //TO DO: Fix Delete, only 25 items in google list and I should also ask outlook if item still exists...
+                foreach (CalendarEvent cEvent in eventsNotProcessed)
+                {
+                    //gManager.DeleteEvent(cEvent.Id);
+                }
+            }
+
+#if DEBUG
+            Console.WriteLine("Press any key to continue ...");
+            Console.ReadLine();
+#endif
+        }
+
+        /// <summary>
+        /// Reads the app.config and fill's in the variables
+        /// </summary>
+        private static void ReadConfig()
+        {
+            Console.WriteLine("Configuratie inlezen");
+
+            user = ConfigurationManager.AppSettings["email"];
+            pass = ConfigurationManager.AppSettings["pass"];
+            calendarId = ConfigurationManager.AppSettings["calendarId"];
+        }
+    }
+}
