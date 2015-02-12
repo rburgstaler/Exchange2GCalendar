6c610d42f24249fca7ad499b9b98bd7bfab20413
Rick Burgstaler <rburgstaler@yahoo.com>
2015-02-11 22:54:21 -0600

    Remove *hated* commented out code

--- Ex2GCal/Ex2GCalForm.cs
+++ Ex2GCal/Ex2GCalForm.cs
@@ -72,24 +72,6 @@ namespace Ex2GCal
                 
             }
 
-            /*
-Event event = new Event();
-event.setSummary("Appointment");
-event.setLocation("Somewhere");
-
-ArrayList<EventAttendee> attendees = new ArrayList<EventAttendee>();
-attendees.add(new EventAttendee().setEmail("attendeeEmail"));
-// ...
-event.setAttendees(attendees);
-
-Date startDate = new Date();
-Date endDate = new Date(startDate.getTime() + 3600000);
-DateTime start = new DateTime(startDate, TimeZone.getTimeZone("UTC"));
-event.setStart(new EventDateTime().setDateTime(start));
-DateTime end = new DateTime(endDate, TimeZone.getTimeZone("UTC"));
-event.setEnd(new EventDateTime().setDateTime(end));
-             */
-
             Event entry = new Event();
 
             entry.Summary = "Event Summary";
