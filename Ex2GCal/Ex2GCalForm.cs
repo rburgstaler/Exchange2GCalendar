161354a22b56faf09acc5018d3b8fe73df343a7b
Rick Burgstaler <rburgstaler@yahoo.com>
2015-02-12 22:40:49 -0600

    Move more synchronization specific logic out of the forms app and into the assembly

--- Ex2GCal/Ex2GCalForm.cs
+++ Ex2GCal/Ex2GCalForm.cs
@@ -242,59 +242,7 @@ namespace Ex2GCal
 
         private void PerformSynch()
         {
-
-            CalendarManagerGoogle gManager = new CalendarManagerGoogle(tbClientID.Text, tbClientSecret.Text, tbCalendar.Text);
-
-            //List<CalendarEvent> events = CalendarManagerOutlook.GetAllEvents();
-            ThreadMsg("Getting all events from Outlook Calendar");
-            List<CalendarEvent> eventsInExchange = (new CalendarManagerExchange() { Status = ThreadMsg}).GetAllEvents(tbExchangeURL.Text, tbExchangeUserName.Text, tbExchangePassword.Text);
-            ThreadMsg("Found {0} events in the Outlook calendar", eventsInExchange.Count);
-
-            List<CalendarEvent> eventsInGoogle = gManager.GetAllEvents();
-
-            //Error check to make sure we do not need to implement paging
-            if (eventsInGoogle.Count == CalendarGlobals.MaxGoogleEntriesToReturn) throw new Exception("Google event feed paging needs to be implemented.");
-            ThreadMsg("Found {0} events in the google calendar", eventsInGoogle.Count);
-
-            CalendarEvent searchEvent;
-
-            foreach (CalendarEvent cEvent in eventsInExchange)
-            {
-                //searchEvent = gManager.GetEventFromSyncId(cEvent.Id);
-                searchEvent = eventsInGoogle.Find(t => t.Id.Equals(cEvent.Id));
-
-                if (searchEvent == null)
-                {   // -> create a new event
-                    ThreadMsg("Creating event \"{0}\" ... ", cEvent.Subject);
-                    gManager.CreateEvent(cEvent);
-                    ThreadMsg("Created");
-                }
-                else if (cEvent.IsChanged(searchEvent))
-                {
-                    // -> update event
-                    ThreadMsg("Update event \"{0}\" ... ", cEvent.Subject);
-                    gManager.UpdateEvent(cEvent);
-                    ThreadMsg("Updated");
-                }
-
-            }
-
-            //Find all events that exist in Google that do not exist in Exchange
-            List<CalendarEvent> deleteList = new List<CalendarEvent>();
-            foreach (CalendarEvent googleEvent in eventsInGoogle)
-            {
-                if (eventsInExchange.Exists(t => t.Id.Equals(googleEvent.Id))) continue;
-                deleteList.Add(googleEvent);
-
-            }
-
-            ThreadMsg("Found {0} events needing deletion in google", deleteList.Count);
-            foreach (CalendarEvent googleEvent in deleteList)
-            {
-                ThreadMsg("Deleting event \"{0}\" {1} ... ", googleEvent.Subject, googleEvent.StartDate);
-                gManager.DeleteEvent(googleEvent.Id);
-            }
-
+            Synch.PerformSynch(ThreadMsg, tbClientID.Text, tbClientSecret.Text, tbCalendar.Text, tbExchangeURL.Text, tbExchangeUserName.Text, tbExchangePassword.Text);
             //We are done so enable the synch button
             ThreadProc(
                 delegate()
---
+++ LibEx2GCal/Synch.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibEx2GCal
+{
+    public delegate void SynchMessage(String Msg, params Object[] list);
+    public class Synch
+    {
+
+        static public void PerformSynch(SynchMessage msgCallback, String tbClientID, String tbClientSecret, String tbCalendar, String tbExchangeURL, String tbExchangeUserName, String tbExchangePassword)
+        {
+
+            CalendarManagerGoogle gManager = new CalendarManagerGoogle(tbClientID, tbClientSecret, tbCalendar);
+
+            //List<CalendarEvent> events = CalendarManagerOutlook.GetAllEvents();
+            msgCallback("Getting all events from Outlook Calendar");
+            List<CalendarEvent> eventsInExchange = (new CalendarManagerExchange() { Status = new ExchangeStatus(msgCallback) }).GetAllEvents(tbExchangeURL, tbExchangeUserName, tbExchangePassword);
+            msgCallback("Found {0} events in the Outlook calendar", eventsInExchange.Count);
+
+            List<CalendarEvent> eventsInGoogle = gManager.GetAllEvents();
+
+            //Error check to make sure we do not need to implement paging
+            if (eventsInGoogle.Count == CalendarGlobals.MaxGoogleEntriesToReturn) throw new Exception("Google event feed paging needs to be implemented.");
+            msgCallback("Found {0} events in the google calendar", eventsInGoogle.Count);
+
+            CalendarEvent searchEvent;
+
+            foreach (CalendarEvent cEvent in eventsInExchange)
+            {
+                //searchEvent = gManager.GetEventFromSyncId(cEvent.Id);
+                searchEvent = eventsInGoogle.Find(t => t.Id.Equals(cEvent.Id));
+
+                if (searchEvent == null)
+                {   // -> create a new event
+                    msgCallback("Creating event \"{0}\" ... ", cEvent.Subject);
+                    gManager.CreateEvent(cEvent);
+                    msgCallback("Created");
+                }
+                else if (cEvent.IsChanged(searchEvent))
+                {
+                    // -> update event
+                    msgCallback("Update event \"{0}\" ... ", cEvent.Subject);
+                    gManager.UpdateEvent(cEvent);
+                    msgCallback("Updated");
+                }
+
+            }
+
+            //Find all events that exist in Google that do not exist in Exchange
+            List<CalendarEvent> deleteList = new List<CalendarEvent>();
+            foreach (CalendarEvent googleEvent in eventsInGoogle)
+            {
+                if (eventsInExchange.Exists(t => t.Id.Equals(googleEvent.Id))) continue;
+                deleteList.Add(googleEvent);
+
+            }
+
+            msgCallback("Found {0} events needing deletion in google", deleteList.Count);
+            foreach (CalendarEvent googleEvent in deleteList)
+            {
+                msgCallback("Deleting event \"{0}\" {1} ... ", googleEvent.Subject, googleEvent.StartDate);
+                gManager.DeleteEvent(googleEvent.Id);
+            }
+
+        }
+    }
+}
