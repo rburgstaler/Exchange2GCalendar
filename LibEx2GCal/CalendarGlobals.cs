85a2c4f0516e5189e617ad5a59d40f36717366a3
Rick Burgstaler <rburgstaler@yahoo.com>
2015-02-12 22:56:19 -0600

    Put in the realistic Google max # of entries that can be returned

--- LibEx2GCal/CalendarGlobals.cs
+++ LibEx2GCal/CalendarGlobals.cs
@@ -7,7 +7,7 @@ namespace LibEx2GCal
 {
     public class CalendarGlobals
     {
-        public static int MaxGoogleEntriesToReturn = 999999;
+        public static int MaxGoogleEntriesToReturn = 2400;  //Actual Google max of v3 interface is 2500
         public static DateTime StartDate = DateTime.Now.AddMonths(-6);
         public static DateTime EndDate = DateTime.Now.AddMonths(6);
     }
