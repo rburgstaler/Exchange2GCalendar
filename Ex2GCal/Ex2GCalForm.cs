896cfb2ba2a0978013e3ffdbecb628d2e6fc171e
Rick Burgstaler <rburgstaler@yahoo.com>
2015-02-17 22:13:28 -0600

    Remove unused using clauses!

--- Ex2GCal/Ex2GCalForm.cs
+++ Ex2GCal/Ex2GCalForm.cs
@@ -1,22 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
+﻿using Google.Apis.Auth.OAuth2;
 using Google.Apis.Calendar.v3;
 using Google.Apis.Calendar.v3.Data;
 using Google.Apis.Services;
-using Google.Apis.Auth.OAuth2;
 using Google.Apis.Util.Store;
+using LibEx2GCal;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using LibEx2GCal;
+using System.Windows.Forms;
 
 namespace Ex2GCal
 {
