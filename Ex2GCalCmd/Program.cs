5c12f6ad55c38293ba0fbdef2d5797e804998cff
Rick Burgstaler <rburgstaler@yahoo.com>
2015-02-17 22:19:43 -0600

    Beginnings of getting the command line app to work similar to the forms app.

--- Ex2GCalCmd/Program.cs
+++ Ex2GCalCmd/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibEx2GCal;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,12 @@ namespace Ex2GCalCmd
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            SynchParams par = new SynchParams();
+            par.Load();
+            Synch.PerformSynch(Console.WriteLine, par);
         }
     }
 }
